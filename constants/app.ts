c18cd4aa2ee16f04513d529e783eca0a9a75fceb
phucle1010 <lethephuc2002@gmail.com>
2025-12-09 15:20:09 +0700

    chore: add env for site url

--- constants/app.ts
+++ constants/app.ts
@@ -1,11 +1,13 @@
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://jobtinda.de";
+
 export const APP_CONFIG = {
   iosTeamId: "FL8C342Y9U",
   iosAppStoreId: "6752797791",
   iosBundleId: "com.sanpot.tinda-mobile-fe",
   androidPackageName: "com.sanpot.tinda_mobile_fe",
   androidPlayStoreUrl: "https://play.google.com/store/apps/details?id=com.sanpot.tinda_mobile_fe",
-  universalLinkScheme: "https://jobtinda.de",
-  websiteDomain: "https://jobtinda.de",
+  universalLinkScheme: SITE_URL,
+  websiteDomain: SITE_URL,
   androidSha256Fingerprints: [
     process.env.SHA256_FINGERPRINT_DEBUG,
     process.env.SHA256_FINGERPRINT_RELEASE,
